b13b6bba608f5cb1aa4b5affca92b0bd9ad8980e
coolJenny <coolJenny009g@gmail.com>
2018-05-21 01:32:20 -0400

    ..

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -11,13 +11,21 @@ var AllItems = React.createClass({
 		this.props.handleDelete(id);
 	},
 
+	handleEdit(){
+
+	},
+
 	render(){
 		var items = this.props.items.map((item) => {
 			return (
 				<div key={item.id}>
-					<h3>{item.name}</h3>
-					<p>{item.description}</p>
-					<button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
+					<Item item={item} 
+							handleDelete={this.handleDelete.bind(this, item.id)}
+							handleEdit={this.handleEdit} />
+					// <h3>{item.name}</h3>
+					// <p>{item.description}</p>
+					// <button onClick={this.handleEdit()}>Edit</button>
+					// <button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
 				</div>
 			);
 		});
