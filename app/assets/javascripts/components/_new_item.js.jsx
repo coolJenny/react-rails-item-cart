d411353cde0d0b2746469a5a7dba680c6db43e4e
coolJenny <coolJenny009g@gmail.com>
2018-05-20 22:19:44 -0400

    ..

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -1,9 +1,27 @@
 var NewItem = React.createClass({
+
+	handleClick(){
+		var name = this.refs.name.value;
+		var description = this.refs.description.value;
+
+		$.ajax({
+			url: "api/v1/items",
+			type: "POST",
+			data: {item: {name: name, description: description}},
+			success: response => {
+				console.log("It worked!", response);
+			}
+		});
+
+	},
+
 	render(){
 		return(
 			<div>
-				<h1>New Item</h1>
+			<input ref='name' placeholder='Enter the name of the item' />
+			<input ref='description' placeholder='Enter a description of the item' />
+			<button onClick={this.handleClick}>Create a New Item</button>
 			</div>
-		);
+			);
 	}
 });
