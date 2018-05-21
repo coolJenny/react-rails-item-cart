ff2e5f496aaececa206660e19b7abe5385ee4010
coolJenny <coolJenny009g@gmail.com>
2018-05-20 22:12:11 -0400

    ..

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,12 +1,25 @@
 var AllItems = React.createClass({
+	getInitialState(){
+		return { items: [] }
+	},
+
 	componentDidMount(){
-		console.log("component mounted");
+		$.getJSON('/api/v1/items.json', (response) => { this.setState({ items: response }) });
 	},
 
 	render(){
+		var items = this.state.items.map((item) => {
+			return (
+				<div key={item.id}>
+					<h3>{item.name}<h3>
+					<p>{item.description}<p>
+				</div>
+			)
+		});
+
 		return(
 			<div>
-				<h1>All items component</h1>
+				{items}
 			</div>
 		);
 	}
